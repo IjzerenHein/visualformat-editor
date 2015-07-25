506285b2069e843de8cac0b2f0884cf5d3cac791
IjzerenHein <hrutjes@gmail.com>
2015-07-25 09:22:04 +0200

    Updated vflToLayout to a single function.

--- src/vflToLayout.js
+++ src/vflToLayout.js
@@ -1,27 +1,27 @@
 var AutoLayout = require('autolayout.js');
 
-function _layout(view, context) {
-    view.setSize(context.size[0], context.size[1]);
-    var subView;
-    for (var key in view.subViews) {
-        if (key.indexOf('_') !== 0) {
-            subView = view.subViews[key];
-            context.set(subView.name, {
-                size: [subView.width, subView.height],
-                translate: [subView.left, subView.top, subView.zIndex * 5]
-            });
-        }
-    }
-}
-
-module.exports = function(visualFormat, options) {
+function vflToLayout(visualFormat, options) {
     var view = new AutoLayout.View(options);
     try {
         var constraints = AutoLayout.VisualFormat.parse(visualFormat, {extended: true, strict: false});
         view.addConstraints(constraints);
-        return _layout.bind(view, view);
+        return function(context) {
+            view.setSize(context.size[0], context.size[1]);
+            var subView;
+            for (var key in view.subViews) {
+                if (key.indexOf('_') !== 0) {
+                    subView = view.subViews[key];
+                    context.set(subView.name, {
+                        size: [subView.width, subView.height],
+                        translate: [subView.left, subView.top, subView.zIndex * 5]
+                    });
+                }
+            }
+        };
     }
     catch (err) {
         console.log(err); //eslint-disable-line no-console
     }
-};
+}
+
+module.exports = vflToLayout;
